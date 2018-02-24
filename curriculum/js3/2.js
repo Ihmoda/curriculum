e5a1a5c15037f917f7fe3fde891520430668b88d
Omar Ihmoda <omarihmoda@DN0a226637.SUNet>
2018-02-24 12:04:11 -0800

    initial commit

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -4,8 +4,20 @@
  * @param return {number}
  */
 
-const solution = (arr)=>{
-  return 0;
+const solution = (arr, idx=1, counterObject={}, mostCommon=arr[0])=>{
+
+  if (Object.keys(counterObject).length === 0) counterObject[arr[0]] = 1
+
+  if (idx === arr.length) return mostCommon
+
+  counterObject[arr[idx]] = arr[idx] in counterObject ? counterObject[arr[idx]] + 1 : 1
+
+  if (counterObject[arr[idx]] > counterObject[mostCommon]){
+    mostCommon = arr[idx];
+  }
+
+  return solution(arr, idx+1, counterObject, mostCommon)
+
 }
 
 module.exports = {
